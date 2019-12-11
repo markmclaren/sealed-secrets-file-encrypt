4b85a7f6b98f8b7520b3489e51fcf2d67e644604
Mark McLaren <mark.mclaren@bristol.ac.uk>
2019-12-11 11:33:34 +0000

    Tidy up

--- test.py
+++ test.py
@@ -17,20 +17,12 @@ for key in data:
         if val.startswith('DEC(') & val.endswith(')'):
             val = val.replace("DEC(", "", 1)  # remove first instance of prefix
             val = val[:val.rfind(")")] # remove suffix
-            data[key] = val # replace value
             print(val)
+            # https://stackoverflow.com/questions/58161224/how-to-use-kubeseal-to-seal-a-helm-templated-secret
             # https://stackoverflow.com/questions/9393425/python-how-to-execute-shell-commands-with-pipe
             # echo -n <secret-password> | kubeseal --raw --scope namespace-wide --from-file=/dev/stdin
-
-            #p1 = subprocess.Popen(["echo", "-n", val], stdout=subprocess.PIPE)
-            #p2 = subprocess.Popen(["kubeseal", "--raw",  "--scope",  "namespace-wide", "--from-file=/dev/stdin"], stdin=p1.stdout, stdout=subprocess.PIPE)
-            #p2.communicate()
-            #print("OUTPUT: " + p2.stdout.read().decode("utf-8") )
-
-            mycmd=subprocess.getoutput('echo -n {} | kubeseal --raw --scope namespace-wide --from-file=/dev/stdin'.format(val))
-            print(mycmd)
-
-            #result = subprocess.run(['ls'], capture_output=True)
-            #print(result.stdout)
+            encryptedValue=subprocess.getoutput('echo -n {} | kubeseal --raw --scope namespace-wide --from-file=/dev/stdin'.format(val))
+            print(encryptedValue)
+            data[key] = encryptedValue # replace value
     print('\n')
 yaml.dump(data, sys.stdout)
