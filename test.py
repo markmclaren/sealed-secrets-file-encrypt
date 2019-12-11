8992bc4473e53d9615b4e7adc6d5faffb4a8c682
Mark McLaren <mark.mclaren@bristol.ac.uk>
2019-12-11 11:35:21 +0000

    More tidying - added support for comments in Yaml file

--- test.py
+++ test.py
@@ -5,24 +5,18 @@ import ruamel.yaml
 import subprocess
 
 yaml = ruamel.yaml.YAML()
-# yaml.preserve_quotes = True
+yaml.preserve_quotes = True
 with open('values.yaml') as fp:
     data = yaml.load(fp)
 for key in data:
     val = data.get(key)
-    print(key)
-    print(val)
-    print(isinstance(val, str))
     if isinstance(val, str):
         if val.startswith('DEC(') & val.endswith(')'):
             val = val.replace("DEC(", "", 1)  # remove first instance of prefix
             val = val[:val.rfind(")")] # remove suffix
-            print(val)
             # https://stackoverflow.com/questions/58161224/how-to-use-kubeseal-to-seal-a-helm-templated-secret
             # https://stackoverflow.com/questions/9393425/python-how-to-execute-shell-commands-with-pipe
             # echo -n <secret-password> | kubeseal --raw --scope namespace-wide --from-file=/dev/stdin
             encryptedValue=subprocess.getoutput('echo -n {} | kubeseal --raw --scope namespace-wide --from-file=/dev/stdin'.format(val))
-            print(encryptedValue)
             data[key] = encryptedValue # replace value
-    print('\n')
 yaml.dump(data, sys.stdout)
