821d18c6777d1fc9c23c408b6c58e14536e8816f
Mark McLaren <mark.mclaren@bristol.ac.uk>
2019-12-11 09:21:24 +0000

    Initial commit

---
+++ README-python.md
@@ -0,0 +1,6 @@
+Use Python (I experimented with Go but it isn't very good at dealing with dyncamic types.  Can use yaml2go but that only gets you so far.
+----
+Read YAML file
+Iterate YAML
+Detected "DEC()" strings
+Execute "kubeseal raw" to encrypt strings and write them back to the YAML
---
+++ test.py
@@ -0,0 +1,23 @@
+import sys
+import ruamel.yaml
+import subprocess
+
+yaml = ruamel.yaml.YAML()
+# yaml.preserve_quotes = True
+with open('values.yaml') as fp:
+    data = yaml.load(fp)
+for key in data:
+    val = data.get(key)
+    print(key)
+    print(val)
+    print(isinstance(val, str))
+    if isinstance(val, str):
+        if val.startswith('DEC(') & val.endswith(')'):
+            val = val.replace("DEC(", "", 1)  # remove first instance of prefix
+            val = val[:val.rfind(")")] # remove suffix
+            data[key] = val # replace value
+            print(val)
+            result = subprocess.run(['ls'], capture_output=True)
+            print(result.stdout)
+    print('\n')
+yaml.dump(data, sys.stdout)
